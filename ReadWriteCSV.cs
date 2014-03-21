d4d58393f072626c74f8a307e2b16232c904fa08
jdkc4d <josh@joshkelahan.com>
2014-03-21 15:23:27 -0500

    Added CSV Class, Added Sample JSON for reference

---
+++ ReadWriteCSV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milage_Matrix_Minder
+{
+    class ReadWriteCSV
+    {
+        //Files
+
+        public String ToJSON(String CSVPath)
+        {
+            return "";
+        }
+
+        public String toCSV(String MyMilageReport)
+        {
+            return "";
+        }
+    }
+}
