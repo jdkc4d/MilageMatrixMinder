81251e30b3ccd0638d616037d4c0fc288ecefdf2
jdkc4d <Ea5t1999>
2014-04-15 07:20:08 -0500

    Created the Office Class to instanciate office data

---
+++ Office.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milage_Matrix_Minder
+{
+    class Office
+    {
+        //This class is for the serialization/deserialization of the json'ed matrix.
+    }
+}
